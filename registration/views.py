6c41661e6c221fec9181d2a5bd9a09e9b18e08b3
Mdaliraza1 <mdaliraza92@gmail.com>
2025-05-08 22:14:33 +0530

    logout added

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, status, exceptions
 from rest_framework.permissions import IsAuthenticated
-from datetime import timedelta,timezone
+from datetime import timedelta, timezone
+from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
