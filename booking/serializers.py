8fcb67308f5dd5cfb840f059f7626ce29afcd52a
Mdaliraza1 <mdaliraza92@gmail.com>
2025-05-10 15:42:33 +0530

    latest update

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,12 +1,12 @@
-
 from rest_framework import serializers
 from .models import Booking
-from registration.models import User  
+from registration.models import User
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+        read_only_fields = ['user']  # So 'user' is not required in input
 
 class AvailableSlotsSerializer(serializers.Serializer):
     date = serializers.DateField()
