88bc93c26a3d43222dfd2cff030dd7df75be6c7b
Mdaliraza1 <mdaliraza92@gmail.com>
2025-05-10 13:18:05 +0530

    latest update

--- booking/urls.py
+++ booking/urls.py
@@ -4,7 +4,7 @@ from .views import CreateBookingView, UserBookingsView, ServiceProviderBookingsV
 
 urlpatterns = [
     path('create/', CreateBookingView.as_view(), name='create-booking'),
-    path('user/', UserBookingsView.as_view(), name='user-bookings'),
-    path('provider/', ServiceProviderBookingsView.as_view(), name='provider-bookings'),
+    path('my-bookings/', UserBookingsView.as_view(), name='user-bookings'),
+    path('provider-bookings/', ServiceProviderBookingsView.as_view(), name='provider-bookings'),
     path('slots/', AvailableSlotsView.as_view(), name='available-slots'),
 ]
