6926721f86b480530c0191369bbef0822096db8d
Mdaliraza1 <mdaliraza92@gmail.com>
2025-05-10 12:46:34 +0530

    latest update

--- fixly/urls.py
+++ fixly/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('registration.urls')),
-    path('',include('service.urls'))
+    path('',include('service.urls')),
+    path('booking/',include('booking.urls'))
 ]
