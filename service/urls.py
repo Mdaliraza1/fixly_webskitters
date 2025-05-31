b566ea13c08ecdca79bd887a1666c001ffdb2b28
Mdaliraza1 <121343030+Mdaliraza1@users.noreply.github.com>
2025-05-31 16:57:20 +0530

    Update urls.py

--- service/urls.py
+++ service/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ServiceListCreateView, ServiceRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('/', ServiceListCreateView.as_view(), name='service-create'),
+    path('', ServiceListCreateView.as_view(), name='service-create'),
     path('<int:pk>/', ServiceRetrieveUpdateDestroyView.as_view(), name='service-retrieve-update-delete'),
 ]
