8ee9b22cc7e22550543325685be4a80b724a23f8
Mdaliraza2 <malidagreat99@gmail.com>
2025-06-13 02:51:36 +0530

    admin panel update

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Booking
+from registration.models import User
 from utils.admin_actions import export_as_csv_action
 
 @admin.register(Booking)
@@ -23,21 +24,27 @@ class BookingAdmin(admin.ModelAdmin):
     ordering = ('-date', '-time_slot')
     actions = [export_as_csv_action()]
 
+    # Editable fields in admin form
     fieldsets = (
         ('Booking Details', {
-            'fields': ('user', 'service_provider', 'date', 'time_slot')
+            'fields': ('user', 'service', 'service_provider', 'date', 'time_slot')
         }),
         ('Status Information', {
             'fields': ('status',),
             'classes': ('collapse',)
         }),
     )
-    fields = ('user', 'service', 'service_provider', 'date', 'time_slot')
-    def get_readonly_fields(self, request, obj=None):
-        if obj:  # editing an existing object
-            return ('user', 'service_provider', 'service', 'date', 'time_slot')
-        return ()
 
+    # Allow editing fields even for existing bookings
+    fields = ('user', 'service', 'service_provider', 'date', 'time_slot', 'status')
+
+    # Optional: Only show SERVICE_PROVIDER in provider dropdown
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "service_provider":
+            kwargs["queryset"] = User.objects.filter(user_type="SERVICE_PROVIDER")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    # Display helpers
     def get_user_name(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
     get_user_name.short_description = 'User Name'
@@ -47,9 +54,11 @@ class BookingAdmin(admin.ModelAdmin):
     get_user_email.short_description = 'User Email'
 
     def get_provider_name(self, obj):
-        return f"{obj.service_provider.first_name} {obj.service_provider.last_name}"
+        if obj.service_provider:
+            return f"{obj.service_provider.first_name} {obj.service_provider.last_name}"
+        return "-"
     get_provider_name.short_description = 'Provider Name'
 
     def get_provider_email(self, obj):
-        return obj.service_provider.email
+        return obj.service_provider.email if obj.service_provider else "-"
     get_provider_email.short_description = 'Provider Email'
