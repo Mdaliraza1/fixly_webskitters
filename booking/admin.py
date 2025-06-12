3a310b773de8c045e7533b995aeb3493df4dcede
Mdaliraza2 <malidagreat99@gmail.com>
2025-06-13 03:39:25 +0530

    admin panel update

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django import forms
 from .models import Booking, User
-from service.models import Service
-from django.utils.html import format_html
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
@@ -16,7 +13,7 @@ class BookingAdmin(admin.ModelAdmin):
         'status',
         'created_at',
     )
-    list_filter = ('status', 'date', 'service')
+    list_filter = ('status', 'date')
     search_fields = (
         'user__email', 'user__first_name', 'user__last_name',
         'service_provider__email', 'service_provider__first_name', 'service_provider__last_name'
@@ -32,11 +29,16 @@ class BookingAdmin(admin.ModelAdmin):
             'fields': ('user', 'service_provider', 'date', 'time_slot', 'status')
         }),
     )
-   
+    readonly_fields = ('user_display', 'user_category_display')
 
+    # Custom display methods
     def get_user_name(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
     get_user_name.short_description = 'User Name'
+
+    def get_user_email(self, obj):
+        return obj.user.email
+    get_user_email.short_description = 'User Email'
 
     def get_user_category(self, obj):
         return obj.user.category
@@ -45,6 +47,10 @@ class BookingAdmin(admin.ModelAdmin):
     def get_provider_name(self, obj):
         return f"{obj.service_provider.first_name} {obj.service_provider.last_name}"
     get_provider_name.short_description = 'Provider Name'
+
+    def get_provider_email(self, obj):
+        return obj.service_provider.email
+    get_provider_email.short_description = 'Provider Email'
 
     def user_display(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
