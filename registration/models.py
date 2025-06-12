9e262cb06e2cecf205ccc546c7d6697b056d9a03
Mdaliraza2 <malidagreat99@gmail.com>
2025-06-13 01:01:09 +0530

    admin panel update

--- registration/models.py
+++ registration/models.py
@@ -71,3 +71,14 @@ class User(AbstractUser):
         verbose_name = _('User')
         verbose_name_plural = _('Users')
         ordering = ['-date_joined']
+
+class UserToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.token[:10]}..."
+
+    class Meta:
+        ordering = ['-created_at']
