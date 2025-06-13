00faf7b951d0a2578432ea4f05e7641099bf5b5b
Mdaliraza2 <malidagreat99@gmail.com>
2025-06-13 11:41:43 +0530

    update

--- booking/admin.py
+++ booking/admin.py
@@ -9,7 +9,7 @@ class ProviderChoiceField(forms.ModelChoiceField):
 class BookingAdminForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['user', 'service_provider', 'service', 'date', 'time_slot', 'status']
+        fields = ['user', 'service_provider', 'date', 'time_slot', 'status']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
