569b5955bac5df0ff34edf719e99d613965e5766
Md Ali Raza <121343030+Mdaliraza1@users.noreply.github.com>
2025-06-19 00:10:34 +0530

    Update urls.py

--- fixly/urls.py
+++ fixly/urls.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', custom_admin_site.urls),
-    path('api/', include('registration.urls')),
+    path('', include('registration.urls')),
     path('services/', include('service.urls')),
     path('review/', include('review.urls')),
     path('booking/', include('booking.urls')),
